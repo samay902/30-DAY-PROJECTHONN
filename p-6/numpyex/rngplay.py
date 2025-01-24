e29a05d20eaa7a9317fecbd920c40e58367f791a
samay902 <samaybasnet@gmail.com>
2025-01-24 12:50:53 +0545

    updated code

--- p-6/numpyex/rngplay.py
+++ p-6/numpyex/rngplay.py
@@ -2,6 +2,9 @@ import numpy as np
 rg=np.random.default_rng(2)
 a=rg.random((2,3))
 print(a)
-a=np.array([2,2,3,2])
+a=np.array([2,2,3,2,4,7,8,9,0,0,8])
 b=np.sin(a)
 print(b)
+d=np.exp(a)
+print(d)
+e=a[5]
