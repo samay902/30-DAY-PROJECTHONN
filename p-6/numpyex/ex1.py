13277d0957932093e5243219f1ed3f89dd98c75b
samay902 <samaybasnet@gmail.com>
2025-02-05 18:37:50 +0545

    misc

--- p-6/numpyex/ex1.py
+++ p-6/numpyex/ex1.py
@@ -10,3 +10,24 @@ print(d)
 print(d.ndim)
 e=d**2
 print(e)
+f=np.eye(5,5)
+print(f)
+g=np.linspace(0,1,10)
+print(g)
+h=np.logspace(0,2,5)
+print(h)
+i=np.random.randint(10,24+1,size=(3,3))
+print(i)
+j=np.random.choice([0,1],10)
+print(j)
+
+
+def multiplier(a):
+    a*=3
+    a+=a
+    a%=2
+    return a
+a=np.array([1,2,3,4])
+print(multiplier(a))
+
+    
---
+++ p-6/test.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+num_atr=["height","weight","iq"]
+cat_atr=["country","religion","caste","city"]
+num_pipeline=make_pipeline(SimpleImputer(strategy="median"),StandardScaler())
+cat_pipeline=make_pipeline(SimpleImputer(strategy="most_frequent"),OneHotEncoder(handle_unknown="ignore"))
+col_trans=ColumnTransformer([
+("num",num_pipeline,num_atr),
+("cat",cat_pipeline,cat_atr),
+])
